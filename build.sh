53ed65cb6674cf530f7004a44576660c30f06900
Herbert Rocha <herberthb12@gmail.com>
2020-08-18 16:57:33 -0400

    Update build
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

---
+++ build.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+echo ""
+echo "Building map2Check-library ... "
+echo ""
+
+cd build
+
+usage()
+{
+    echo "usage: [-r | --release ] | [-d | --debug] | [-t | --travis] | [-h | --help]"
+}
+
+build_release()
+{
+    cmake .. -DCMAKE_INSTALL_PREFIX=../release-library/ -DENABLE_TEST=OFF -DENABLE_COVCODE=OFF -DBUILD_DOC=ON
+    make
+    make install
+}
+
+build_debug()
+{
+    cmake .. -DCMAKE_CXX_COMPILER=/usr/bin/clang++-8 -DCMAKE_INSTALL_PREFIX=../release-library/ -DENABLE_TEST=ON -DENABLE_COVCODE=ON -DBUILD_DOC=OFF
+    cmake --build . -- VERBOSE=1
+    cd test
+    LLVM_PROFILE_FILE="map2check.profraw" ./unit_tests
+    llvm-profdata-8 merge -sparse map2check.profraw -o map2check.profdata
+    llvm-cov-8 report ./unit_tests -instr-profile=map2check.profdata
+}
+
+##### Main
+
+type_builds=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -r | --release )            shift
+                                    type_builds="release"
+                                ;;
+        -d | --debug )              interactive=1
+                                    type_builds="debug"
+                                ;;
+        -t | --travis )             interactive=1
+                                    type_builds="travis"
+                                ;;
+        -h | --help )               usage
+                                    exit
+                                ;;
+        * )                         usage
+                                    exit 1
+    esac
+    shift
+done
+
+
+# Test code to verify command line processing
+if [ "$type_builds" = "debug" ]; then
+	echo "Debug build is on"
+    build_debug
+elif [ "$type_builds" = "debug" ]; then
+	echo "Release build is on"
+else
+    echo "Travis build is on"
+fi
