7fd466da2acb241e9b298847ee5a5b1426b8536e
Herbert Rocha <herberthb12@gmail.com>
2020-08-28 17:03:10 -0400

    Minor changes on travis build
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- run_klee_test.sh
+++ run_klee_test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd build 
 
-docker run --rm -v $(pwd):/home/src:Z klee/klee /bin/bash -c "cd klee_build; klee --libcxx -exit-on-error-type=Abort /home/src/fullprogram.bc"
-docker run --rm -v $(pwd):/home/src:Z klee/klee /bin/bash -c "cd klee_build; ktest-tool /home/src/klee-out-0/test000001.ktest | grep -c \"object 0: name:\" "
+docker run --rm -v $(pwd):/home/src:Z --user $(id -u):$(id -g) klee/klee /bin/bash -c "cd klee_build; klee --libcxx -exit-on-error-type=Abort /home/src/fullprogram.bc"
+docker run --rm -v $(pwd):/home/src:Z --user $(id -u):$(id -g) klee/klee /bin/bash -c "cd klee_build; ktest-tool /home/src/klee-out-0/test000001.ktest | grep -c \"object 0: name:\" "
 if [ $? -eq 0 ]; 
 then
     echo ">>> KLEE OKAY"
