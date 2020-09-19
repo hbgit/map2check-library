9ca88c71c2a99e42262ad45e71070661550777f0
Herbert Rocha <herberthb12@gmail.com>
2020-09-19 18:42:10 -0400

    Add CallerLibraryTrackBbLog
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

---
+++ src/CallerLibraryTrackBbLog.cpp
@@ -0,0 +1,31 @@
+//===-- include/CallerLibraryTrackBbLog.cpp - class definition -------*- C++
+//-*-===//
+//
+// Copyright (C) 2014 - 2020 Map2Check tool
+// This file is part of the Map2Check tool, and is made available under
+// the terms of the GNU General Public License version 3.
+//
+// SPDX-License-Identifier: (GPL-3.0)
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of the CallerWrapperLibrary.cpp class,
+/// which performs the track the basic block for the programa analysis 
+/// by map2check tool.
+///
+//===----------------------------------------------------------------------===//
+
+#include "../include/CallerLibraryTrackBbLog.hpp"
+#include "../include/CallerLibraryResult.hpp"
+
+
+extern "C" bool map2checkIsInTrackedBb(unsigned LineNumber){
+    return ResultCntrTrackBbLog.isInTrackedBbLog(LineNumber);
+}
+
+extern "C" void map2checkAddInTrackedBb(unsigned LineNumber, const char *FunctionName){
+    GlobalTrackBbLog.Line = LineNumber;
+    GlobalTrackBbLog.FunctName = FunctionName;
+    ResultCntrTrackBbLog.ContainerLog_.push_back(GlobalTrackBbLog);
+}
